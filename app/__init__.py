e8f81e0c7d301b972d3ab3e2fe07e2109aac9f18
sudoskys <coldlando@hotmail.com>
2023-12-15 16:07:13 +0800

    feat(init)

--- app/__init__.py
+++ app/__init__.py
@@ -15,10 +15,14 @@ from pydantic import BaseModel, Field
 from app.tagger import WdTaggerSDK
 from .settings import CurrentSetting
 
-aclient = instructor.apatch(AsyncOpenAI(
-    base_url=CurrentSetting.openai_base_url,
-    api_key=CurrentSetting.openai_api_key)
-)
+SYSTEM = """
+你现在是一个电商平台的运营，需要从相关信息和模板中创作出一个商品的标题和介绍。 !important
+
+Study the provided copywriting templates to understand the style and emotional expression each one conveys.
+Once you have grasped the stylistic elements, I will present you with a specific subject.
+Your task will then be to synthesize a new piece of copywriting that aligns with the learned styles.
+Your creation should capture the essence of the new subject, resonate with the themes of the examples, and include suitable hashtags for thematic consistency.
+"""
 
 
 class ProductsIntro(BaseModel):
@@ -31,33 +35,43 @@ class ProductsIntro(BaseModel):
     tags: list[str] = Field(..., description="Exp: ['#风景', '#旅行']")
 
 
+aclient = instructor.apatch(AsyncOpenAI(
+    base_url=CurrentSetting.openai_base_url,
+    api_key=CurrentSetting.openai_api_key)
+)
+
 app = FastAPI()
-
-SYSTEM = """
-你现在是一个电商平台的运营，需要从相关信息和模板中创作出一个商品的标题和介绍。 !important
-
-Study the provided copywriting templates to understand the style and emotional expression each one conveys.
-Once you have grasped the stylistic elements, I will present you with a specific subject.
-Your task will then be to synthesize a new piece of copywriting that aligns with the learned styles.
-Your creation should capture the essence of the new subject, resonate with the themes of the examples, and include suitable hashtags for thematic consistency.
-"""
 
 # Load templates into memory
 with open('template.json', 'r') as f:
-    templates = json.load(f)
+    MESSAGE_TEMPLATE = json.load(f)
 
-logger.info(f"Loaded {len(templates)} templates...")
+logger.info(f"Loaded {len(MESSAGE_TEMPLATE)} templates...")
+
+
+class MappingDefault(dict):
+    def __missing__(self, key):
+        return key
+
+
+def get_template(content, *,
+                 tags: str):
+    return content.format_map(
+        MappingDefault(
+            tags=tags
+        )
+    )
 
 
 @app.get("/templates")
 async def get_templates():
-    return templates
+    return MESSAGE_TEMPLATE
 
 
 @app.post("/generate_caption")
 async def generate_caption(template_id: str, file: UploadFile = File(...)) -> JSONResponse:
     try:
-        user_template = templates[template_id]
+        user_template = MESSAGE_TEMPLATE[template_id]
     except KeyError:
         return JSONResponse(content={"error": "Invalid template_id"}, status_code=400)
     try:
@@ -73,7 +87,8 @@ async def generate_caption(template_id: str, file: UploadFile = File(...)) -> JS
         return JSONResponse(content={"error": "WD API Error"}, status_code=500)
     logger.info(f"Tagging: {raw_input_wd}")
     try:
-        task = (">参考模板\n" + user_template + f"""\n\n>仿写任务\n商品标签：{raw_input_wd}""")
+        task = get_template(content=user_template, tags=raw_input_wd)
+        # task = (">参考模板\n" + user_template + f"""\n\n>仿写任务\n商品标签：{raw_input_wd}""")
         logger.info(f"PromptTask: {task}")
         model = await aclient.chat.completions.create(
             model="gpt-3.5-turbo",
