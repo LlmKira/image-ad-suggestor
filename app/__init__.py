05a71ce40db9321c144dcc1994e5b6d519b18461
sudoskys <coldlando@hotmail.com>
2023-12-15 10:35:11 +0800

    feat(init)

--- app/__init__.py
+++ app/__init__.py
@@ -29,6 +29,22 @@ class ProductsIntro(BaseModel):
 
 app = FastAPI()
 
+SYSTEM = """
+你现在是一个电商平台的运营，需要从相关信息和模板中创作出一个商品的标题和介绍
+Study the provided copywriting templates to understand the style and emotional expression each one conveys.
+Once you have grasped the stylistic elements, I will present you with a specific subject.
+Your task will then be to synthesize a new piece of copywriting that aligns with the learned styles.
+Your creation should capture the essence of the new subject, resonate with the themes of the examples, and include suitable hashtags for thematic consistency.
+"""
+
+USER_TEMPLATE = """
+>Template
+“峰回路转，雾凇乍现，一缕阳光斜射，刹那间晶莹剔透，宛若仙境，美极了！ #风景 #旅行”
+“秋是慢入的,但冷却是突然的,晴不知夏去,一雨方觉秋深！有些情绪不言而喻…… #最美风景 #秋天 #心动的旅行”
+“愿新年胜旧年，愿将来胜过往，愿你与旧事归于尽，来年依旧迎花开。#向烟花许个愿 #除夕 #大年三十 #跨年夜”
+“你是我最想留住的幸运，我想用我的心，把你永远留在我身边。#情话 #表白 #爱情”
+"""
+
 
 @app.post("/generate_caption")
 async def generate_caption(file: UploadFile = File(...)) -> JSONResponse:
@@ -51,25 +67,16 @@ async def generate_caption(file: UploadFile = File(...)) -> JSONResponse:
             messages=[
                 {
                     "role": "system",
-                    "content": """你现在是一个电商平台的运营，需要从相关信息和模板中创作出一个商品的标题和介绍"""
-                               """Study the provided copywriting templates to understand the style and emotional expression each one conveys. """
-                               """Once you have grasped the stylistic elements, I will present you with a specific subject. """
-                               """Your task will then be to synthesize a new piece of copywriting that aligns with the learned styles. """
-                               """Your creation should capture the essence of the new subject, resonate with the themes of the examples, and include suitable hashtags for thematic consistency."""
+                    "content": SYSTEM
                 },
                 {
                     "role": "user",
-                    "content": f"""
-    # Template 
-    “峰回路转，雾凇乍现，一缕阳光斜射，刹那间晶莹剔透，宛若仙境，美极了！ #风景 #旅行”
-    “秋是慢入的,但冷却是突然的,晴不知夏去,一雨方觉秋深！有些情绪不言而喻…… #最美风景 #秋天 #心动的旅行”
-    “愿新年胜旧年，愿将来胜过往，愿你与旧事归于尽，来年依旧迎花开。#向烟花许个愿 #除夕 #大年三十 #跨年夜”
-
-    # 仿写任务
-    Tags：{raw_input_wd}
-
-    """
-                    # f"Desc：{_blip_input_} , Tags：{raw_input_wd}"
+                    "content":
+                        USER_TEMPLATE
+                        + f"""
+                        >仿写任务
+                        Tags：{raw_input_wd}
+                        """
                 },
             ],
         )
